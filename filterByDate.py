29a3c629c478b9b4a60e1e0d91e721b741286d48
DiegoVargas <diego.vargas@deutschebahn.com>
2022-06-06 14:59:47 +0200

    Adjusted year functionality

--- filterByDate.py
+++ filterByDate.py
@@ -2,56 +2,58 @@
 import pandas as pd
 import os
 import glob
+
+year="2019"
  
-# path = "csv"
-# allFiles = glob.glob(os.path.join(path, "*.csv"))
+path = year+"_csv"
+allFiles = glob.glob(os.path.join(path, "*.csv"))
 
-# for pFile in allFiles:
-#     fileName = pFile.split('\\')[-1]
-#     print("Ready to filter "+fileName)
+for pFile in allFiles:
+    fileName = pFile.split('\\')[-1]
+    print("Ready to filter "+fileName)
 
-#     # read file
-#     dataFrame = pd.read_csv(pFile)
+    # read file
+    dataFrame = pd.read_csv(pFile)
 
-#     # # convert to datetime
-#     dataFrame['DatetimeBegin'] = pd.to_datetime(dataFrame['DatetimeBegin'])
+    # # convert to datetime
+    dataFrame['DatetimeBegin'] = pd.to_datetime(dataFrame['DatetimeBegin'])
 
-#     # # calculate mask
-#     mask = dataFrame['DatetimeBegin'].between('2020-04-01', '2020-04-30')
+    # # calculate mask
+    mask = dataFrame['DatetimeBegin'].between(year+'-04-01', year+'-04-30')
 
-#     # # output masked dataframes
-#     dataFrame[mask].to_csv("csv_april_filtered/"+fileName+"_filtered.csv", index=False)
-#     print("Done with filter "+fileName)
+    # # output masked dataframes
+    dataFrame[mask].to_csv(year+"_csv_april_filtered/"+fileName+"_filtered.csv", index=False)
+    print("Done with filter "+fileName)
 
 
 
 #### Calculates the average per point
 
-# path = "csv_filtered"
-# allFilteredFiles = glob.glob(os.path.join(path, "*.csv"))
+path = year+"_csv_april_filtered"
+allFilteredFiles = glob.glob(os.path.join(path, "*.csv"))
 
-# for pFile in allFilteredFiles:
-#     # get file name
-#     fileName = pFile.split('\\')[-1]
+for pFile in allFilteredFiles:
+    # get file name
+    fileName = pFile.split('\\')[-1]
 
-#     # read file
-#     dataFrame = pd.read_csv(pFile)
+    # read file
+    dataFrame = pd.read_csv(pFile)
 
-#     # Calculate mask
-#     newDataFrame = dataFrame.groupby(['AirQualityStation', 'UnitOfMeasurement'])['Concentration'].agg(['mean']).reset_index()
+    # Calculate mask
+    newDataFrame = dataFrame.groupby(['AirQualityStation', 'UnitOfMeasurement'])['Concentration'].agg(['mean']).reset_index()
 
-#     #calculate mean concentration
-#     newDataFrame.to_csv("csv_mean_concentration/"+fileName+"_mean_concentration.csv", index=False)
-#     print("saves mean concentration for "+fileName)
+    #calculate mean concentration
+    newDataFrame.to_csv(year+"_csv_mean_concentration/"+fileName+"_mean_concentration.csv", index=False)
+    print("saves mean concentration for "+fileName)
 
 
 #### concatenates all filtered files
 
-# path = "csv_mean_concentration"
-# allFilteredFiles = glob.glob(os.path.join(path, "*.csv"))
+path = year+"_csv_mean_concentration"
+allFilteredFiles = glob.glob(os.path.join(path, "*.csv"))
 
-# for pFile in allFilteredFiles:
-#     print("Concatenates "+pFile.split('\\')[-1])
-#     dataFrame = pd.concat((pd.read_csv(f) for f in allFilteredFiles))
+for pFile in allFilteredFiles:
+    print("Concatenates "+pFile.split('\\')[-1])
+    dataFrame = pd.concat((pd.read_csv(f) for f in allFilteredFiles))
 
-# dataFrame.to_csv("csv_single_file_filtered/csv_filtered.csv", index=False)
+dataFrame.to_csv(year+"_csv_single_file_filtered/csv_filtered.csv", index=False)
